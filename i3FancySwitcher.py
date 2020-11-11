02fc97eabc26923397294c25dd4fed557190e524
wtheisen <wtheisen@nd.edu>
2020-11-11 10:49:56 -0500

    Switched to PySimpleGUI(bad idea?), made a bar instead of fullscreen

--- i3FancySwitcher.py
+++ i3FancySwitcher.py
@@ -1,4 +1,5 @@
 import tcolors, cv2, numpy, math
+import PySimpleGUI as sg
 from PIL import Image, ImageDraw, ImageFont
 from i3ipc import Connection, Event
 
@@ -7,8 +8,10 @@ icon_dict = {
     'chrome':'',
     'slack':'聆',
     'spotify':'',
-    'vim': ''
+    'vim': '',
+    'unknown': ''
 }
+
 ws_pos_dict = {}
 i3 = Connection()
 
@@ -34,70 +37,86 @@ for c in tcolors.get_xcolors('/home/wtheisen/.cache/wal/colors.Xresources', '*')
 def show_image(img):
     img.show()
 
-def create_ws_matte(apps, ws_w, ws_h, ws_name, ws_x_offset, ws_y_offset, main_matte, scale):
+def create_ws_matte(apps, ws_w, ws_h, ws_name, scale):
     global icon_dict
     global ws_pos_dict
     global color_dict
 
     fnt =  ImageFont.truetype('/home/wtheisen/Downloads/Literation Mono Bold Nerd Font Complete.ttf', 64)
+    ws_matte = Image.new('RGBA', (ws_w, ws_h), color = color_dict['background'] + (0,))
+    draw_matte = ImageDraw.Draw(ws_matte)
 
-    def draw_app_rect(x, y, w, h, g, s, x_o, y_o):
-        x = int(x * s) + x_o
-        y = int(y * s) + y_o
+    def draw_app_rect(x, y, w, h, g, s):
+        x = int(x * s)
+        y = int(y * s)
         w = int(w * s)
         h = int(h * s)
 
-        main_matte.rectangle([x, y, x + w, y + h], fill = color_dict['background'] + (255,))
-        main_matte.text((int(x + w / 2) - 20, int(y + h / 2) - 20), g, font=fnt, fill = color_dict['color5'] + (255,))
+        draw_matte.rectangle([x, y, x + w, y + h], fill = color_dict['background'] + (255,))
+        draw_matte.text((int(x + w / 2) - 20, int(y + h / 2) - 20), g, font=fnt, fill = color_dict['color5'] + (255,))
 
     for a in apps:
         r = a.rect
         if 'vim' in a.name:
-            draw_app_rect(r.x, r.y, r.width, r.height, icon_dict['vim'], scale, ws_x_offset, ws_y_offset)
+            draw_app_rect(r.x, r.y, r.width, r.height, icon_dict['vim'], scale)
         elif 'Term' in a.name:
-            draw_app_rect(r.x, r.y, r.width, r.height, icon_dict['term'], scale, ws_x_offset, ws_y_offset)
+            draw_app_rect(r.x, r.y, r.width, r.height, icon_dict['term'], scale)
         elif 'Chrome' in a.name or 'chrom' in a.name:
-            draw_app_rect(r.x, r.y, r.width, r.height, icon_dict['chrome'], scale, ws_x_offset, ws_y_offset)
-        elif 'Spotify' in a.name:
-            draw_app_rect(r.x, r.y, r.width, r.height, icon_dict['spotify'], scale, ws_x_offset, ws_y_offset)
+            draw_app_rect(r.x, r.y, r.width, r.height, icon_dict['chrome'], scale)
+        elif 'spotify' in a.name:
+            draw_app_rect(r.x, r.y, r.width, r.height, icon_dict['spotify'], scale)
         elif 'Slack' in a.name:
-            draw_app_rect(r.x, r.y, r.width, r.height, icon_dict['slack'], scale, ws_x_offset, ws_y_offset)
+            draw_app_rect(r.x, r.y, r.width, r.height, icon_dict['slack'], scale)
         else:
-            print('App type not found')
+            draw_app_rect(r.x, r.y, r.width, r.height, icon_dict['unknown'], scale)
+
+    ws_matte.save(f'ws_{ws_name}_matte.png')
+    return f'ws_{ws_name}_matte.png'
+
+def create_ws_buttons(ws_matte_filename_list, t_rect, orient):
+    if orient == 'vert':
+        layout_list = []
+    else:
+        layout_list = [[]]
+
+    for ws_matte in ws_matte_filename_list:
+        ws_name = ''.join(ws_matte.split('_')[1:-1])
+        btn = sg.Button('', image_filename = ws_matte,
+                image_size=(int(t_rect.width * scale), int(t_rect.height * scale)),
+                key=ws_name,)
+                # button_color=(sg.theme_background_color(), sg.theme_background_color()))
+        if orient == 'vert':
+            layout_list.append([btn])
+        else:
+            layout_list[0].append(btn)
+
+    return layout_list
+
+#################################
 
 tree = i3.get_tree()
-
 t_rect = tree.rect
-main_matte = Image.new('RGBA', (t_rect.width, t_rect.height), color = color_dict['background'] + (0,))
-draw_matte = ImageDraw.Draw(main_matte)
 
-x_offset = 0
-y_offset = int(t_rect.height / 2) - 250
 num_ws = len(tree.workspaces())
 rows = math.ceil(num_ws / 3)
 scale = 1 / num_ws
 print(f'Rows: {rows}, Scale: {scale}')
 
+ws_matte_filename_list = []
 for ws in tree.workspaces():
     r = ws.rect
-    x = int(r.x * scale) + x_offset
-    y = int(r.y * scale) + y_offset
+    x = int(r.x * scale)
+    y = int(r.y * scale)
     w = int(r.width * scale)
     h = int(r.height * scale)
     ws_pos_dict[ws.name] = (x, y, w, h)
 
-    draw_matte.rectangle([x, y, x + w, y + h], outline = color_dict['color1'] + (255,), fill = color_dict['foreground'] + (255,), width = 4)
-    create_ws_matte(ws.leaves(), ws.rect.width, ws.rect.height, ws.name, x_offset, y_offset, draw_matte, scale)
-    x_offset += int(ws.rect.width * scale)
+    ws_matte_filename_list.append(create_ws_matte(ws.leaves(), w, h, ws.name, scale))
 
-img = numpy.array(main_matte)
-img = img[:, :, ::-1].copy()
-
-window_name = 'image'
-
-cv2.namedWindow(window_name, cv2.WND_PROP_FULLSCREEN)
-cv2.moveWindow(window_name, t_rect.x - 1, t_rect.y - 1)
-cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-cv2.imshow(window_name, img)
-cv2.setMouseCallback(window_name, mouse_click)
-cv2.waitKey(0)
+orient = 'hor'
+ws_button_list = create_ws_buttons(ws_matte_filename_list, t_rect, orient)
+w = sg.Window('i3FancySwitcher', ws_button_list, alpha_channel = 1.0)
+events, values = w.read()
+print(f'Clicked on {events}')
+w.close()
+i3.command(f'workspace {events}')
